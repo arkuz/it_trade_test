c04331f289fe1d0dfa901561953561fcd50bcd76
arkuz <arkuzka@mail.ru>
2019-03-19 13:49:48 +0300

    replaced \n on <br>

--- get_params.php
+++ get_params.php
@@ -2,6 +2,6 @@
 	if(!empty($_REQUEST)){
 		echo '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />';
 		foreach ($_REQUEST as $key => $value) {
-			echo "$key: $value \n";
+			echo "$key: $value <br>";
 		}
 	} else echo '$_REQUEST is empty';
