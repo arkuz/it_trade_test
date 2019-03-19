265ae06656bae78cc8da1355cafe6a40b4569090
arkuz <arkuzka@mail.ru>
2019-03-19 13:49:29 +0300

    added meta tag

--- get_params.php
+++ get_params.php
@@ -1,5 +1,6 @@
 <?php
 	if(!empty($_REQUEST)){
+		echo '<meta http-equiv="content-type" content="text/html; charset=UTF-8" />';
 		foreach ($_REQUEST as $key => $value) {
 			echo "$key: $value \n";
 		}
